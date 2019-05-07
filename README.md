ad680781e5e52713f3a63676d5a98422cfeef64e
Erlend Mongstad <eeerlend@gmail.com>
2019-05-07 18:13:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# macup-homebrew-casks
+
+A [macup](https://github.com/eeerlend/macup) module that installs Homebrew casks on your mac. Dependant on [macup-homebrew](https://github.com/eeerlend/macup-homebrew) 
+
+## Installation
+Run the following command to add it to your repo
+
+```bash
+npm install eeerlend/macup-homebrew-casks --save
+```
+
+## Configuration
+Add your own packages to be installed in the macup configuration file like this...
+
+```bash
+macup_homebrew_casks+=(
+  spotify
+)
+```
---
+++ dist/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function install_or_upgrade_cask {
+  if brew cask ls --versions ${1} > /dev/null; then
+		report_from_package "${1} already installed."
+	else
+		report_from_package " Installing cask ${1}"
+		brew cask install ${1} --appdir=/Applications
+	fi
+}
+
+# Install Homebrew casks
+# todo: check if array is declared up front!
+# shellcheck disable=SC2154
+for ((i=0; i<${#macup_homebrew_casks[@]}; ++i)); do
+	install_or_upgrade_cask "${macup_homebrew_casks[i]}"
+done
+
+# Update outdated casks
+brew cask upgrade
